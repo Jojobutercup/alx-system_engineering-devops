464e73f8c6fbca53131be8b5b04fecc42fda069c
Osariemen Uwuilehue <106764970+Jojobutercup@users.noreply.github.com>
2023-01-13 04:00:54 -0800

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,11 @@
-
+# create a file school in /tmp
+file {'/tmp/school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+Footer
+© 2023 GitHub, Inc. 
